ea63cd0d6138d160309ef60cea24a1a85c36c12f
Elena Hanna <hanna_elena@live.se>
2024-09-13 12:51:48 +0000

    Add initial structure for signupform component

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import axios from "axios";
+
+const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
