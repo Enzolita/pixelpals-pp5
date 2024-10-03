5004f777ee0a02bcac5c8439939fd439b0a61066
Elena Hanna <hanna_elena@live.se>
2024-10-03 10:36:11 +0000

    Configure axios instances

--- src/api/axiosDefault.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = 'https://pixelpals-pp5-ee2d5ecf265c.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,20 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://pixelpals-backend-49bb71efec81.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
+
+export const setAuthorizationHeader = (data) => {
+  if (data?.access) {
+    localStorage.setItem('access_token', data.access);
+    localStorage.setItem('refresh_token', data.refresh);
+    axiosReq.defaults.headers.common["Authorization"] = `Bearer ${data.access}`;
+    axiosRes.defaults.headers.common["Authorization"] = `Bearer ${data.access}`;
+  } else {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    delete axiosReq.defaults.headers.common["Authorization"];
+    delete axiosRes.defaults.headers.common["Authorization"];
+  }
+};
