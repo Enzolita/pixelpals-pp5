f6a5df0abab9a3013c3d01f049b3a7bc4a3960fa
Elena Hanna <hanna_elena@live.se>
2024-10-04 15:25:53 +0000

    Add PostEditForm component for editing posts

---
+++ src/pages/posts/PostEditForm.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+import { useHistory, useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
+function PostEditForm() {
+  const [errors, setErrors] = useState({});
+
+  const [postData, setPostData] = useState({
+    title: "",
+    content: "",
+    image: "",
+  });
+  const { title, content, image } = postData;
+
+  const imageInput = useRef(null);
+  const history = useHistory();
+  const { id } = useParams();
+
