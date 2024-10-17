4bc165ca11a986f892733f0d3b31b5578d516250
Elena Hanna <hanna_elena@live.se>
2024-10-17 09:11:04 +0000

    Add CustomModal

---
+++ src/components/CustomModal.js
@@ -0,0 +1,31 @@
+// Custom code
+
+import React from "react";
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
+import { useHistory } from "react-router-dom";
+
+function CustomModal({ show, handleClose, title, message, redirectPath }) {
+  const history = useHistory();
+
+  const handleCloseAndRedirect = () => {
+    handleClose();
+    history.push(redirectPath);
+  };
+
+  return (
+    <Modal show={show} onHide={handleCloseAndRedirect}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{message}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleCloseAndRedirect}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default CustomModal;
